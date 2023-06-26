d9e08b1abedb6b363f5a096a301d9c49efd0f254
brian mosagwe <bramosbrian@gmail.com>
2023-06-26 15:48:47 -0700

    prints every character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
+/**
+ * puts2 - print one char out of 2 of a string
+ * @str: char array string type
+ * Description: Print 1st char, then 3rd, then 5th, etc..
+ */
+
 void puts2(char *str)
 {
-	if (str == NULL)
-		return;
+	int i;
 
-	int index = 0;
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
