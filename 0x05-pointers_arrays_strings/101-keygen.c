139c06b5f6d83acfd1fe71fd514afc4d9956efbb
brian mosagwe <bramosbrian@gmail.com>
2023-06-26 16:46:45 -0700

    any message

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 6
+
+void generate_password(char *password, int length)
+{
+    static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int i;
+
+    for (i = 0; i < length; i++)
+    {
+        int index = rand() % (sizeof(charset) - 1);
+        password[i] = charset[index];
+    }
+
+    password[length] = '\0';
+}
+
+int main(void)
+{
+    char password[PASSWORD_LENGTH + 1];
+    int attempts = 0;
+
+    srand(time(NULL));
+
+    do
+    {
+        generate_password(password, PASSWORD_LENGTH);
+        printf("Attempt #%d: %s\n", attempts + 1, password);
+        attempts++;
+    } while (strcmp(password, "A-<8t4") != 0);
+
+    printf("Password cracked after %d attempts!\n", attempts);
+
+    return 0;
+}
