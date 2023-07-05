20deaa6aec1d283bdc6d60b07887fe066089471c
brian mosagwe <bramosbrian@gmail.com>
2023-07-05 16:15:40 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,58 +1,48 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+bool wildcmp_helper(char *s1, char *s2);
 
 /**
- * wildcmp - compare two strings with "wildcard expansion" capabilities
- * @s1: string 1
- * @s2: string 2
- * Return: 1 if strings can be considered identical, else 0
+ * wildcmp - Compares two strings and returns 1 if they can be considered identical.
+ * @s1: The first string to compare.
+ * @s2: The second string to compare.
+ *
+ * Return: 1 if the strings can be considered identical, 0 otherwise.
  */
-
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	else if (*s1 == '\0' || *s2 == '\0')
-	{
-		if (*s1 == '\0' && *s2 == '*')
-			return wildcmp(s1, ++s2);
-		else if (*s1 == '*' && *s2 == '\0')
-			return wildcmp(++s1, s2);
-		return (0);
-	}
-
-	if (*s1 == *s2)
-	{
-		return wildcmp(++s1, ++s2);
-	}
-	else if (*s1 == '*')
-	{
-		if (*(s1 + 1) == '*')
-			return wildcmp(++s1, s2);
-		else
-		{
-			return wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2);
-		}
-	}
-	else if (*s2 == '*')
-	{
-		if (*(s2 + 1) == '*')
-			return wildcmp(s1, ++s2);
-		else
-		{
-			return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
-		}
-	}
-
-	return (0);
-
+	return wildcmp_helper(s1, s2);
 }
 
-int findsrc(char *s, char c, int i, int p)
+/**
+ * wildcmp_helper - Recursive helper function to compare two strings.
+ * @s1: The first string to compare.
+ * @s2: The second string to compare.
+ *
+ * Return: 1 if the strings can be considered identical, 0 otherwise.
+ */
+bool wildcmp_helper(char *s1, char *s2)
 {
-	if (*(s + i) == '\0')
-		return (p + 1);
-	else if (*(s + i) == c || *(s + i) == '*')
-		p = i;
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return true; /* Base case: Both strings are empty, they are identical */
+	}
+	else if (*s1 == *s2 || *s2 == '*')
+	{
+		if (*s1 != '\0')
+		{
+			if (wildcmp_helper(s1 + 1, s2 + 1))
+			{
+				return true; /* Characters match or s2 has '*', continue comparing */
+			}
+		}
 
-	return (findsrc(s, c, i + 1, p));
+		if (*s2 == '*' && wildcmp_helper(s1, s2 + 1))
+		{
+			return true; /* Try to match '*' with empty string or continue matching */
+		}
+	}
+
+	return false; /* Strings are not identical */
 }
