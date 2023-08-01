50b0909bd3556a7f4c5361d498e50f7cea3699f8
brian mosagwe <bramosbrian@gmail.com>
2023-08-01 06:53:37 -0700

    list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a list
+ * @h: pointer to the list_t
+ *
+ * Return: number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t g;
+       
+	g= 0;
+
+	while (h)
+	{
+		g++;
+		h = h->next;
+	}
+	return (g);
+}
