f3b5c320127d395e598439d1dfc7844204921b67
brian mosagwe <bramosbrian@gmail.com>
2023-08-01 08:52:37 -0700

    hello holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+    hello db 'Hello, Holberton', 0
+    fmt db '%s', 10, 0
+
+section .text
+    extern printf
+
+global main
+
+main:
+    push rbp
+    mov rdi, fmt
+    mov rsi, hello
+    xor rax, rax
+    call printf
+    pop rbp
+    ret
