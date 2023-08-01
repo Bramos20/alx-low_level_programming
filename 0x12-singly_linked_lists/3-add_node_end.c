930bafb376d0230e7992f46e90644964c1e7ec2e
brian mosagwe <bramosbrian@gmail.com>
2023-08-01 07:46:53 -0700

    add node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,46 +1,41 @@
-#include "lists.h"
+#include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
+#include "lists.h"
 
 /**
- * add_node_end - Adds a new node at the end of a `list_t` list.
- * @head: Double pointer to the head of the list.
- * @str: String to be duplicated into the new node.
+ * add_node_end - Adds a new node at the end of a linked list.
+ * @head: Double pointer to the list_t list.
+ * @str: String to put in the new node.
  *
- * Return: Address of the new element or NULL if it fails.
+ * Return: Address of the new element, or NULL if it failed.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *last;
-    list_t *new_elem;
-    int str_len = 0;
+	list_t *new_node;
+	list_t *temp_node = *head;
+	unsigned int str_len = 0;
 
-    last = *head;
-    while (last && last->next != NULL)
-        last = last->next;
+	while (str[str_len])
+		str_len++;
 
-    while (str[str_len] != '\0')
-        str_len++;
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
 
-    new_elem = malloc(sizeof(list_t));
-    if (new_elem == NULL)
-    {
-        free(new_elem);
-        return (NULL);
-    }
-    new_elem->str = strdup(str);
-    if (new_elem->str == NULL)
-    {
-        free(new_elem);
-        return (NULL);
-    }
-    new_elem->len = str_len;
-    new_elem->next = NULL;
+	new_node->str = strdup(str);
+	new_node->len = str_len;
+	new_node->next = NULL;
 
-    if (last)
-        last->next = new_elem;
-    else
-        *head = new_elem;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    return (new_elem);
+	while (temp_node->next)
+		temp_node = temp_node->next;
+
+	temp_node->next = new_node;
+
+	return (new_node);
 }
